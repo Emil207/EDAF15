343b1d6408a59fa6ac992b980ab03ec8ff39d063
WilhelmLund <wilhelm.lundstroms@gmail.com>
2018-05-11 15:53:28 +0200

    development in algorithm for project

--- project/fast/fast.c
+++ project/fast/fast.c
@@ -5,7 +5,7 @@ bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows]
  	struct rational a2[rows][cols];
 	struct rational c2[rows];
 	size_t tmp = 0, tmp2;
-	size_t n1 = 0, n2 = 0;
+	size_t n1 = 0, n2 = 0, n3 = 0;
 
 	//sort according to m value (positive)
 	for(size_t i = 0; i < rows; i++){
@@ -50,9 +50,28 @@ bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows]
 				c2[tmp].a = c[i];
 				c2[tmp].b = tmp2;
 			}
+			n3 = tmp;
 			tmp++;
 		}
 	}
+
+	struct rational cNew[(n1+1)*(n2-n1)+(n3-n2)];
+	struct rational aNew[(n1+1)*(n2-n1)+(n3-n2)][cols-1];
+	for(size_t i = 0; i <= n1; i++){
+		for(size_t j = n1+1; i <= n2; i++){
+			for (size_t k = 0; k < cols-1; k++){
+				aNew[i*(n2-n1)+j][k] = subq(a[i][k], a[j][k]);
+			}
+			cNew[i*(n2-n1)+j] = subq(c[i], c[j])
+		}
+	}
+	for(size_t i = n2+1; i < (n3-n2); i++){
+		for (size_t k = 0; k < cols-1; k++){
+			aNew[(n1+1)*(n2-n1)-(n2+1)+i][k] = a[i][k];
+		}
+		cNew[(n1+1)*(n2-n1)-(n2+1)+i] = c[i];
+	}
+
 
 	for(size_t i = 0; i < rows; i++){
 		for(size_t j= 0; j < cols; j++){
