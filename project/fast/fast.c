95f0ca3352ea699aa73eecd9468225c8f5791ced
WilhelmLund <wilhelm.lundstroms@gmail.com>
2018-05-21 11:33:43 +0200

    fast update

--- project/fast/fast.c
+++ project/fast/fast.c
@@ -55,31 +55,46 @@ bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows]
 		}
 	}
 
-	struct rational cNew[(n1+1)*(n2-n1)+(n3-n2)];
-	struct rational aNew[(n1+1)*(n2-n1)+(n3-n2)][cols-1];
-	for(size_t i = 0; i <= n1; i++){
-		for(size_t j = n1+1; i <= n2; i++){
-			for (size_t k = 0; k < cols-1; k++){
-				aNew[i*(n2-n1)+j][k] = subq(a[i][k], a[j][k]);
-			}
-			cNew[i*(n2-n1)+j] = subq(c[i], c[j])
-		}
-	}
-	for(size_t i = n2+1; i < (n3-n2); i++){
-		for (size_t k = 0; k < cols-1; k++){
-			aNew[(n1+1)*(n2-n1)-(n2+1)+i][k] = a[i][k];
-		}
-		cNew[(n1+1)*(n2-n1)-(n2+1)+i] = c[i];
-	}
-
-
-	for(size_t i = 0; i < rows; i++){
-		for(size_t j= 0; j < cols; j++){
-			printf("\[%d/%d\]", a2[i][j].a, a2[i][j].b);
-		}
-		printf("\n", '-');
-	}
-	printf("\n", '-');
 
 	return 1;
+}
+
+bool algorithm(size_t rows, size_t cols, struct rational a[rows][cols], struct rational c[rows], size_t n1, size_t n2, size_t n3){
+    if(cols == 1){
+      size_t br = -;
+      for(size_t i = 0; i < n1; i++){
+
+      }
+    }
+
+  	struct rational cNew[(n1+1)*(n2-n1)+(n3-n2)];
+  	struct rational aNew[(n1+1)*(n2-n1)+(n3-n2)][cols-1];
+
+    // extracting variables from positives and negatives
+  	for(size_t i = 0; i <= n1; i++){
+  		for(size_t j = n1+1; i <= n2; i++){
+        printf("test \n", '-');
+  			for (size_t k = 0; k < cols-1; k++){
+  				aNew[i*(n2-n1)+j][k] = subq(a2[i][k], a2[j][k]);
+  			}
+  			cNew[i*(n2-n1)+j] = subq(c2[i], c2[j]);
+  		}
+  	}
+
+    // extracting variables from zero values
+  	for(size_t i = n2+1; i < (n3-n2); i++){
+  		for (size_t k = 0; k < cols-1; k++){
+  			aNew[(n1+1)*(n2-n1)-(n2+1)+i][k] = a2[i][k];
+  		}
+  		cNew[(n1+1)*(n2-n1)-(n2+1)+i] = c2[i];
+  	}
+
+    // printing matrix
+  	for(size_t i = 0; i < rows; i++){
+  		for(size_t j= 0; j < cols; j++){
+  			printf("\[%d/%d\]", a2[i][j].a, a2[i][j].b);
+  		}
+  		printf("\n", '-');
+  	}
+  	printf("\n", '-');
 }
